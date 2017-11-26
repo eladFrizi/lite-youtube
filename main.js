0c595333e83e56e9032ec68aee1c17144a25bb4d
eladFrizi <okc.elad.35@gmail.com>
2017-11-26 19:24:35 +0200

    set-up

---
+++ main.js
@@ -0,0 +1,54 @@
+'use-strict'
+const API_KEY = 'AIzaSyDffDzg6TlfpOXNhc3lmjXHtLiNsoA7PRM'
+
+
+
+async function initApp() {
+    console.log('in the  run')
+    var videos = await searchVideos('surf')
+    renderVideos(videos)
+    changePlayedVideo(videos[0].id.videoId)
+}
+
+
+async function searchVideos(value) {
+    var url = `https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${API_KEY}&q=${value}`;
+    var videos = await fetchVideos(url);
+    console.log(videos, 'asd')
+    return videos
+}
+
+function fetchVideos(url) {
+    return fetch(url)
+        .then(results => results.json())
+        .then(data =>  data.items)
+}
+
+
+function renderVideos(videos) {
+    document.querySelector('#videos-list').innerHTML = videos.map(video => {
+        let snippet = video.snippet
+        let title = snippet.title;
+        let thumbnail = snippet.thumbnails.default.url;
+        let videoId = video.id.videoId
+        return `
+            <div class="video-preview" onclick="changePlayedVideo('${videoId}')">
+            <img class="img-fluid" src="${thumbnail}">
+                <h6>${title}</h6>
+            </div>
+        `
+    })
+}
+
+
+async function searchPressed() {
+    event.preventDefault()
+    var videos = await searchVideos(event.target.value) 
+    renderVideos(videos) 
+}
+
+function changePlayedVideo(videoId) {
+    var elVideoContianer = document.querySelector('.video-container')
+    var elVideoPlayer = elVideoContianer.querySelector('iframe')
+    elVideoPlayer.src = `https://www.youtube.com/embed/${videoId}`
+}
