4232c3dde4f606ef2a8b44f42ff398d130d5847f
eladFrizi <okc.elad.35@gmail.com>
2017-11-26 21:30:45 +0200

    removed graph music and added wiki

--- graphMusic.js
+++ graphMusic.js
@@ -2,7 +2,7 @@ const GM_KEY = '325dbb09f70dfd64fc45f4505b18a73e'
 
 // http://api.musicgraph.com/api/v2/artist/search?api_key=c8303e90962e3a5ebd5a1f260a69b138&similar_to=Pearl+Jam
 function searchSingers(value) {
-    const url = `https://api.musicgraph.com/api/v2/artist/search?api_key=${GM_KEY}&name=${value}`
+    const url = `http://api.musicgraph.com/api/v2/artist/search?api_key=${GM_KEY}&name=${value}`
     return axios.get(url).then(res => res.data.data)
 }
 
